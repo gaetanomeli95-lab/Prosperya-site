a81ef40d0fa69cf9e7b9915a6e3a6d70616cfeba
Gaetano Meli <gaetano.meli95@gmail.com>
2026-08-29 12:15:51 +0200

    seo: enrich organization and website structured data

--- src/components/JsonLd.tsx
+++ src/components/JsonLd.tsx
@@ -1,31 +1,76 @@
-import { company } from '@/data/company';
+import { company, site } from '@/data/company';
 
 export function OrganizationJsonLd() {
-  const json = {
+  const graph = {
     '@context': 'https://schema.org',
-    '@type': 'ProfessionalService',
-    name: company.ragioneSociale,
-    description:
-      'Prosperya affianca imprenditori, aziende e investitori in governance, strategia, controllo, crescita e sviluppo internazionale.',
-    url: company.sito,
-    address: {
-      '@type': 'PostalAddress',
-      streetAddress: company.sedeLegale,
-      addressLocality: 'Altavilla Milicia',
-      postalCode: company.cap,
-      addressRegion: company.regione,
-      addressCountry: 'IT',
-    },
-    telephone: company.telefono,
-    email: company.emailDirezione,
-    vatID: company.pivaCf,
-    sameAs: [company.sito],
+    '@graph': [
+      {
+        '@type': 'Organization',
+        '@id': `${site.url}/#organization`,
+        name: company.ragioneSociale,
+        url: site.url,
+        email: company.emailDirezione,
+        telephone: company.telefono,
+        vatID: company.pivaCf,
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: company.sedeLegale,
+          addressLocality: company.comune,
+          postalCode: company.cap,
+          addressRegion: company.regione,
+          addressCountry: 'IT',
+        },
+      },
+      {
+        '@type': 'ProfessionalService',
+        '@id': `${site.url}/#professional-service`,
+        name: company.ragioneSociale,
+        url: site.url,
+        parentOrganization: { '@id': `${site.url}/#organization` },
+        description: site.description,
+        telephone: company.telefono,
+        email: company.emailDirezione,
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: company.sedeLegale,
+          addressLocality: company.comune,
+          postalCode: company.cap,
+          addressRegion: company.regione,
+          addressCountry: 'IT',
+        },
+        areaServed: [
+          { '@type': 'Country', name: 'Italia' },
+          { '@type': 'AdministrativeArea', name: 'Europa' },
+          { '@type': 'AdministrativeArea', name: 'Area mediterranea' },
+        ],
+        knowsAbout: [
+          'Governance',
+          'Controllo di gestione',
+          'Strategia e crescita',
+          'Start Up',
+          'Creazione NewCo',
+          'Internazionalizzazione',
+          'Ristrutturazione debiti',
+          'Finanza agevolata',
+          'Operazioni straordinarie',
+        ],
+      },
+      {
+        '@type': 'WebSite',
+        '@id': `${site.url}/#website`,
+        url: site.url,
+        name: company.ragioneSociale,
+        description: site.description,
+        publisher: { '@id': `${site.url}/#organization` },
+        inLanguage: 'it-IT',
+      },
+    ],
   };
 
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(json) }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(graph) }}
     />
   );
 }
