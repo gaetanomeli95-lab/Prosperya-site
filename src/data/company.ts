697e78cf2fceb015f0fcbe89d71179c23c6bc335
Gaetano Meli <gaetano.meli95@gmail.com>
2026-09-01 13:05:50 +0200

    content: update WhatsApp contact number

--- src/data/company.ts
+++ src/data/company.ts
@@ -9,7 +9,10 @@ export const company = {
   emailPubblica: 'info.prosperya@gmail.com',
   emailDirezione: 'direzione@prosperya.it',
   emailPrivacy: 'info.prosperya@gmail.com',
-  telefono: '+39 352 023 5382',
+  telefono: '+39 388 253 7532',
+  whatsappNumber: '393882537532',
+  whatsappHref: 'https://wa.me/393882537532',
+  gmailComposeHref: 'https://mail.google.com/mail/?view=cm&fs=1&to=info.prosperya@gmail.com',
   areaOperativa: 'Capo Zafferano, Altavilla Milicia, a meno di 20 km da Palermo',
   sito: 'https://prosperya.it',
 };
